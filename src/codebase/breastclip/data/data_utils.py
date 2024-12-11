43dc03ab13dbdfb865ebf7d5ef447958bf07ac90
duyxxd <duyxxd@umich.edu>
2024-12-11 17:48:38 -0500

    fix

--- src/codebase/breastclip/data/data_utils.py
+++ src/codebase/breastclip/data/data_utils.py
@@ -1,8 +1,56 @@
 import os
-from typing import Dict
+from typing import Dict, Iterable
+import numpy as np
+import cv2
+from PIL import Image
 
 from albumentations import *
 from transformers import AutoTokenizer
+
+
+def otsu_mask(img):
+    median = np.median(img)
+    _, thresh = cv2.threshold(img, median, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+    return thresh
+
+
+class OtsuCut(object):
+
+    def __init__(self, align_orientation: bool = False, remove_text: bool = False):
+        super().__init__()
+        self.algn_orientation = align_orientation
+        self.remove_text = remove_text
+
+    def __process__(self, x):
+        if isinstance(x, Image.Image):
+            x = np.array(x)
+        
+        mask = otsu_mask(cv2.cvtColor(x, cv2.COLOR_RGB2GRAY))
+        # Convert to NumPy array if not already
+
+        # Check if the matrix is empty or has no '1's
+        if mask.size == 0 or not np.any(mask):
+            return Image.fromarray(x)
+
+        # Find the rows and columns where '1' appears
+        rows = np.any(mask == 255, axis=1)
+        cols = np.any(mask == 255, axis=0)
+
+        # Find the indices of the rows and columns
+        min_row, max_row = np.where(rows)[0][[0, -1]]
+        min_col, max_col = np.where(cols)[0][[0, -1]]
+
+        # Crop and return the submatrix
+        x = x[min_row:max_row+1, min_col:max_col+1]
+        
+        img = Image.fromarray(x)
+        return img
+
+    def __call__(self, x):
+        if isinstance(x, Iterable):
+            return [self.__process__(im) for im in x]
+        else:
+            return self.__process__(x)
 
 
 def load_tokenizer(source, pretrained_model_name_or_path, cache_dir, **kwargs):
@@ -65,3 +113,9 @@ def load_transform(split: str = "train", transform_config: Dict = None):
             return Compose([
                 Resize(width=transforms["Resize"]["size_h"], height=transforms["Resize"]["size_w"])
             ])
+        else:
+            return Compose([
+                OtsuCut(),
+                Resize(width=transforms["Resize"]["size_h"], height=transforms["Resize"]["size_w"])
+            ])
+
