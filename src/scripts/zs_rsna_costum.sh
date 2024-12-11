d6ee3f3cffa88d6b2264272a5fe0ff7fe43abad4
duyxxd <duyxxd@umich.edu>
2024-12-11 18:23:27 -0500

    dev

--- src/scripts/zs_rsna_costum.sh
+++ src/scripts/zs_rsna_costum.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
 python ./src/codebase/eval_zero_shot_clip.py --config-name zs_clip.yaml hydra.run.dir="./src/codebase/outputs/zs_pred" model.clip_check_point="./src/codebase/outputs/Mammo-CLIP_ckpt/b5-model-best-epoch-7.tar"
+
+python ./src/codebase/eval_zero_shot_clip.py --config-name zs_clip.yaml hydra.run.dir="./src/codebase/outputs/zs_pred" model.clip_check_point="./src/codebase/outputs/Mammo-CLIP_ckpt/b2-model-best-epoch-10.tar"
