fbde2a6418ccc2ca1ec6929e0866aa9e6fac2ef7
XYPB <yuexi.du@yale.edu>
2025-02-11 23:59:02 -0500

    dev

--- src/scripts/zs_embed_costum.sh
+++ src/scripts/zs_embed_costum.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
-python ./src/codebase/eval_zero_shot_clip.py --config-name zs_clip_embed.yaml hydra.run.dir="./src/codebase/outputs/zs_pred" model.clip_check_point="./src/codebase/outputs/Mammo-CLIP_ckpt/b5-model-best-epoch-7.tar"
+python ./src/codebase/eval_zero_shot_clip.py --config-name zs_clip_embed_birads.yaml hydra.run.dir="./src/codebase/outputs/zs_pred" model.clip_check_point="./src/codebase/outputs/Mammo-CLIP_ckpt/b5-model-best-epoch-7.tar"
 
-python ./src/codebase/eval_zero_shot_clip.py --config-name zs_clip_embed.yaml hydra.run.dir="./src/codebase/outputs/zs_pred" model.clip_check_point="./src/codebase/outputs/Mammo-CLIP_ckpt/b2-model-best-epoch-10.tar"
+python ./src/codebase/eval_zero_shot_clip.py --config-name zs_clip_embed_birads.yaml hydra.run.dir="./src/codebase/outputs/zs_pred" model.clip_check_point="./src/codebase/outputs/Mammo-CLIP_ckpt/b2-model-best-epoch-10.tar"
+
+python ./src/codebase/eval_zero_shot_clip.py --config-name zs_clip_embed_density.yaml hydra.run.dir="./src/codebase/outputs/zs_pred" model.clip_check_point="./src/codebase/outputs/Mammo-CLIP_ckpt/b5-model-best-epoch-7.tar"
+
+python ./src/codebase/eval_zero_shot_clip.py --config-name zs_clip_embed_density.yaml hydra.run.dir="./src/codebase/outputs/zs_pred" model.clip_check_point="./src/codebase/outputs/Mammo-CLIP_ckpt/b2-model-best-epoch-10.tar"
