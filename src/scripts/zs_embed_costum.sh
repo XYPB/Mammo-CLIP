bdac73f0353de7968bb99da322aee3c696e4f168
XYPB <yuexi.du@yale.edu>
2025-02-21 16:52:35 -0500

    fix

--- src/scripts/zs_embed_costum.sh
+++ src/scripts/zs_embed_costum.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-python ./src/codebase/eval_zero_shot_clip.py --config-name zs_clip_embed_birads_screen.yaml hydra.run.dir="./src/codebase/outputs/zs_pred" model.clip_check_point="./src/codebase/outputs/Mammo-CLIP_ckpt/b5-model-best-epoch-7.tar"
+python ./src/codebase/eval_zero_shot_clip.py --config-name zs_clip_embed_birads_screen.yaml hydra.run.dir="./src/codebase/outputs/b5_birads_screen_zs_pred" model.clip_check_point="./src/codebase/outputs/Mammo-CLIP_ckpt/b5-model-best-epoch-7.tar"
 
-python ./src/codebase/eval_zero_shot_clip.py --config-name zs_clip_embed_birads_screen.yaml hydra.run.dir="./src/codebase/outputs/zs_pred" model.clip_check_point="./src/codebase/outputs/Mammo-CLIP_ckpt/b2-model-best-epoch-10.tar"
+python ./src/codebase/eval_zero_shot_clip.py --config-name zs_clip_embed_birads_screen.yaml hydra.run.dir="./src/codebase/outputs/b2_birads_screen_zs_pred" model.clip_check_point="./src/codebase/outputs/Mammo-CLIP_ckpt/b2-model-best-epoch-10.tar"
 
-python ./src/codebase/eval_zero_shot_clip.py --config-name zs_clip_embed_birads.yaml hydra.run.dir="./src/codebase/outputs/zs_pred" model.clip_check_point="./src/codebase/outputs/Mammo-CLIP_ckpt/b5-model-best-epoch-7.tar"
+python ./src/codebase/eval_zero_shot_clip.py --config-name zs_clip_embed_birads.yaml hydra.run.dir="./src/codebase/outputs/b5_birads_zs_pred" model.clip_check_point="./src/codebase/outputs/Mammo-CLIP_ckpt/b5-model-best-epoch-7.tar"
 
-python ./src/codebase/eval_zero_shot_clip.py --config-name zs_clip_embed_birads.yaml hydra.run.dir="./src/codebase/outputs/zs_pred" model.clip_check_point="./src/codebase/outputs/Mammo-CLIP_ckpt/b2-model-best-epoch-10.tar"
+python ./src/codebase/eval_zero_shot_clip.py --config-name zs_clip_embed_birads.yaml hydra.run.dir="./src/codebase/outputs/b2_birads_zs_pred" model.clip_check_point="./src/codebase/outputs/Mammo-CLIP_ckpt/b2-model-best-epoch-10.tar"
 
-python ./src/codebase/eval_zero_shot_clip.py --config-name zs_clip_embed_density.yaml hydra.run.dir="./src/codebase/outputs/zs_pred" model.clip_check_point="./src/codebase/outputs/Mammo-CLIP_ckpt/b5-model-best-epoch-7.tar"
+python ./src/codebase/eval_zero_shot_clip.py --config-name zs_clip_embed_density.yaml hydra.run.dir="./src/codebase/outputs/b5_density_zs_pred" model.clip_check_point="./src/codebase/outputs/Mammo-CLIP_ckpt/b5-model-best-epoch-7.tar"
 
-python ./src/codebase/eval_zero_shot_clip.py --config-name zs_clip_embed_density.yaml hydra.run.dir="./src/codebase/outputs/zs_pred" model.clip_check_point="./src/codebase/outputs/Mammo-CLIP_ckpt/b2-model-best-epoch-10.tar"
+python ./src/codebase/eval_zero_shot_clip.py --config-name zs_clip_embed_density.yaml hydra.run.dir="./src/codebase/outputs/b2_density_zs_pred" model.clip_check_point="./src/codebase/outputs/Mammo-CLIP_ckpt/b2-model-best-epoch-10.tar"
